cf35d8a2965f46f9762971a68ef581e43a5bef57
clauret <clauret@localhost>
2010-05-25 12:48:27 +1000

    Added function to get the MIME type for a file

---
+++ src/org/weborganic/berlioz/util/FileUtils.java
@@ -0,0 +1,77 @@
+package org.weborganic.berlioz.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.weborganic.berlioz.GlobalSettings;
+import org.weborganic.berlioz.logging.ZLogger;
+import org.weborganic.berlioz.logging.ZLoggerFactory;
+
+/**
+ * A bunch of utility functions for files.
+ * 
+ * @author Christophe Lauret
+ * @version 25 May 2010
+ */
+public final class FileUtils {
+
+  /**
+   * Displays debug information.
+   */
+  private static final ZLogger LOGGER = ZLoggerFactory.getLogger(FileUtils.class);
+
+  /**
+   * The MIME properties mapping file extensions to MIME types. 
+   */
+  private static final Properties MIME = new Properties(); 
+
+  /** Utility classes need no constructor. */
+  private FileUtils() {}
+
+  /**
+   * Returns the MIME type for the given file.
+   * 
+   * <p>The MIME type is only based on the file extension.
+   * 
+   * <p>This method uses the the 'mime.properties' resource file from the classpath which maps
+   * file extensions to MIME type. This file is loaded once. 
+   * 
+   * @param f The file.
+   * @return the corresponding MIME type or <code>null</code> if not found.
+   */
+  public static String getMIMEType(File f) {
+    // Load if empty
+    if (MIME.isEmpty()) loadMIMEProperties();
+    // Lookup extension in properties file
+    String name = f.getName();
+    int dot = name.lastIndexOf(".");
+    if (dot >= 0) {
+      return MIME.getProperty(name.substring(dot+1));
+    } else {
+      return null;
+    }
+  }
+
+  // Private helpers ------------------------------------------------------------------------------
+
+  /**
+   * Loads the properties.
+   * 
+   * @return Properties. Always.
+   */
+  private static synchronized void loadMIMEProperties() {
+    File file = new File(GlobalSettings.getRepository(), "config/mime.properties");
+    try {
+      LOGGER.info("Loading MIME properties from {}", file.getAbsolutePath());
+      Class<FileUtils> c = FileUtils.class;
+      InputStream in = c.getResourceAsStream("/mime.properties");
+      MIME.load(in);
+      in.close();
+    } catch (IOException ex) {
+      LOGGER.warn("Unable to read conf properties for template", ex);
+    }
+  }
+
+}
---
+++ src/org/weborganic/berlioz/util/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A collection of utility classes.
+ */
+package org.weborganic.berlioz.util;
