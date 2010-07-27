fdd25f717d833dcb19270e0977fb731f813500c8
clauret <clauret@localhost>
2010-07-27 16:23:07 +1000

    Refactor MD5 algorithm + improve performance

--- src/org/weborganic/berlioz/util/MD5.java
+++ src/org/weborganic/berlioz/util/MD5.java
@@ -7,9 +7,16 @@
  */
 package org.weborganic.berlioz.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import org.slf4j.LoggerFactory;
 
 /**
  * A utility class providing a simple method to generate MD5 hash values for text content.
@@ -22,14 +29,14 @@ import java.security.NoSuchAlgorithmException;
 public final class MD5 {
 
   /**
-   * The length of the hashed value.
-   */
-  private static final int HASH_VALUE_LENGTH = 32;
-
-  /**
    * Character set for the hashing function.
    */
   private static final Charset UTF8 = Charset.forName("utf-8");
+
+  /**
+   * Stores the hex character for easy retrieval.
+   */
+  private static final char[] HEX = "0123456789abcdef".toCharArray();
 
   /**
    * Prevents creation of instance.
@@ -39,22 +46,68 @@ public final class MD5 {
 
   /**
    * Returns a hash value for the specified text.
-   * 
+   *
    * @param text The text value to hash.
    * 
    * @return The {@link HashMap} value for the specified test or <code>null</code> if an error occurred.
+   * 
+   * @throws UnsupportedOperationException If the MD5 algorithm is not available for that platform.
    */
-  public static String hash(String text) {
-    byte[] md5hash = new byte[HASH_VALUE_LENGTH];
-    try {
-      MessageDigest md = MessageDigest.getInstance("MD5");
-      md.update(text.getBytes(UTF8), 0, text.length());
-      md5hash = md.digest();
-    } catch (NoSuchAlgorithmException ex) {
-      ex.printStackTrace();
-    }
-    return toHex(md5hash);
+  public static String hash(String text) throws UnsupportedOperationException {
+    MessageDigest md = getAlgorithm();
+    md.update(text.getBytes(UTF8), 0, text.length());
+    byte[] bytes = md.digest();
+    return toHex(bytes);
   }
+
+  /**
+   * Returns a hash value for the specified file.
+   * 
+   * @param file The file to read
+   * @return The MD5 checksum value as a string.
+   * @throws IOException If the file does not exist or an error occurred while reading the file.
+   * @throws UnsupportedOperationException If the MD5 algorithm is not available for that platform.
+   */
+  public static String hash(File file) throws IOException, UnsupportedOperationException {
+    MessageDigest md = getAlgorithm();
+    FileChannel in = new FileInputStream(file).getChannel();
+    try {
+      MappedByteBuffer buffer = in.map(FileChannel.MapMode.READ_ONLY, 0, in.size());
+      md.update(buffer);
+      byte[] bytes = md.digest();
+      return toHex(bytes);
+    } finally {
+      in.close();
+    }
+  }
+
+  /**
+   * Returns a hash value for the specified file.
+   * 
+   * @param file The file to read
+   * @return The MD5 checksum value as a string.
+   * @throws IOException If the file does not exist or an error occurred while reading the file.
+   * @throws UnsupportedOperationException If the MD5 algorithm is not available for that platform.
+  public static String hash(File file) throws IOException, UnsupportedOperationException {
+    long length = file.length();
+    byte[] buffer = new byte[length > 8096? 8096 : (int)length];
+    MessageDigest md = getAlgorithm();
+    InputStream in = new FileInputStream(file);
+    try {
+      int len;
+      do {
+        len = in.read(buffer);
+        if (len > 0) {
+          md.update(buffer, 0, len);
+        }
+      } while (len != -1);
+    } finally {
+      in.close();
+    }
+    byte[] bytes = md.digest();
+    return toHex(bytes);
+  }
+  */
 
   // Private helpers
   // ----------------------------------------------------------------------------------------------
@@ -66,19 +119,27 @@ public final class MD5 {
    * @return the corresponding sequence of hexadecimal characters.
    */
   private static String toHex(byte[] data) {
-    StringBuffer buf = new StringBuffer();
-    for (int i = 0; i < data.length; i++) {
-      int halfbyte = (data[i] >>> 4) & 0x0F;
-      int two_halfs = 0;
-      do {
-        if ((0 <= halfbyte) && (halfbyte <= 9))
-          buf.append((char)('0' + halfbyte));
-        else
-          buf.append((char)('a' + (halfbyte - 10)));
-        halfbyte = data[i] & 0x0F;
-      } while (two_halfs++ < 1);
+    if (data == null) return null;
+    final StringBuilder hex = new StringBuilder(2 * data.length);
+    for (final byte b : data) {
+      hex.append(HEX[(b & 0xF0) >> 4]).append(HEX[(b & 0x0F)]);
     }
-    return buf.toString();
+    return hex.toString();
+  }
+
+  /**
+   * Returns the MD5 algorithm throwing an unchecked exception if the algorithm is not available.
+   * 
+   * @return the MD5 algorithm.
+   * @throws UnsupportedOperationException Wrapping any occurring 'NoSuchAlgorithmException'.
+   */
+  private static MessageDigest getAlgorithm() throws UnsupportedOperationException {
+    try {
+      return MessageDigest.getInstance("MD5");
+    } catch (NoSuchAlgorithmException ex) {
+      LoggerFactory.getLogger(MD5.class).warn("MD5 algorithm not available:", ex);
+      throw new UnsupportedOperationException(ex);
+    }
   }
 
 }
