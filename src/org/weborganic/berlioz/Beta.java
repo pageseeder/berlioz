847d9c85d0ea7e167093dec74ee0096d11850950
clauret <clauret@localhost>
2010-08-11 10:44:14 +1000

    Added annotation for use on experimental code.

---
+++ src/org/weborganic/berlioz/Beta.java
@@ -0,0 +1,12 @@
+package org.weborganic.berlioz;
+
+/**
+ * When this annotation is used, it indicates that the code is not mature and may be subject to
+ * changes or removal.
+ * 
+ * @author Christophe Lauret
+ * @version 11 August 2010
+ */
+public @interface Beta {
+
+}
