156ead9bece445a03f588de70490c1c10234472e
tyenki <tyenki@gmail.com>
2011-06-27 19:50:00 +1000

    Class for supporting dynamic parameters

---
+++ src/org/weborganic/berlioz/content/ParameterTemplate.java
@@ -0,0 +1,175 @@
+package org.weborganic.berlioz.content;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.weborganic.berlioz.Beta;
+
+/**
+ * Defines a simple template for parameter values. 
+ * 
+ * @author Christophe Lauret
+ * @version 28 June 2011
+ * @since Berlioz 0.8.2
+ */
+@Beta public final class ParameterTemplate {
+
+  /**
+   * A variable in a template.
+   */
+  private static final Pattern VARIABLE = Pattern.compile("\\{[^}]+}");
+
+  /**
+   * The template split in tokens.
+   */
+  private final Token[] _tokens;
+
+  /**
+   * Creates a new parameter template.
+   * 
+   * @param tokens The tokens that make up this template.
+   */
+  private ParameterTemplate(Token[] tokens) {
+    this._tokens = tokens;
+  }
+
+  /**
+   * Resolves this template using the specified content request.
+   * 
+   * @param map A map of values to use.
+   * @return the resolved template as a string.
+   */
+  public String toString(Map<String, String> map) {
+    StringBuilder value = new StringBuilder();
+    for (Token t : this._tokens) {
+      value.append(t.toString(map));
+    }
+    return value.toString();
+  }
+
+  /**
+   * Returns the value of this template using the content request.
+   * 
+   * @param template The template to parse.
+   * @return the resolved template.
+   */
+  public static ParameterTemplate parse(String template) {
+    Matcher m = VARIABLE.matcher(template);
+    List<Token> tokens = new ArrayList<Token>();
+    int start = 0;
+    while (m.find()) {
+      if (m.start() > start) {
+        String text = template.substring(start, m.start());
+        tokens.add(Token.parse(text));
+      }
+      tokens.add(Token.parse(m.group()));
+      start = m.end();
+    }
+    if (start < template.length()) {
+      String text = template.substring(start, template.length());
+      tokens.add(Token.parse(text));
+    }
+    return new ParameterTemplate(tokens.toArray(new Token[]{}));
+  }
+
+  // Token
+  // ----------------------------------------------------------------------------------------------
+
+  /**
+   * A token which can be resolved using a content request. 
+   */
+  private static class Token {
+
+    /** The types of tokens which can be used with this template */
+    private enum Type {
+
+      /** The token is a static string */
+      STRING,
+
+      /** The token is a parameter */
+      PARAMETER
+    }
+
+    /**
+     * The type of token.
+     */
+    private final Type _type;
+
+    /**
+     * The token content.
+     */
+    private final String _name;
+
+    /**
+     * The token content.
+     */
+    private final String _text;
+
+    /**
+     * Creates a new token. 
+     * @param type the type of token
+     * @param name the name of the parameter (may be <code>null</code>.
+     * @param text the text or default value.
+     */
+    public Token(Type type, String name, String text) {
+      this._type = type;
+      this._name = name;
+      this._text = text;
+    }
+
+    /**
+     * Return the resolved value of this token.
+     * @param map the map of values
+     * @return the resolved value of this token. 
+     */
+    private String toString(Map<String, String> map) {
+      switch (this._type) {
+        case STRING: 
+          return this._text;
+        case PARAMETER:
+          String value = map.get(this._name); 
+          return value != null? value : this._text;
+        default:
+          return this._text;
+      }
+    }
+
+    /**
+     * Parses the specified token.
+     * 
+     * @param token the token to parse
+     * @return the corresponding token instance.
+     */
+    public static Token parse(String token) {
+      if (token == null || token.isEmpty()) return null;
+      Type type = Type.STRING;
+      String name = null;
+      String text = token;
+      if (token.length() > 1 && token.charAt(0) == '{' && token.charAt(token.length()-1) == '}') {
+        int eq = token.indexOf('=');
+        // Same as a static token
+        if (eq == 1) {
+          text = token.substring(2, token.length()-1);
+        } else if (eq > 1) {
+          type = Type.PARAMETER;
+          name = token.substring(1, eq);
+          text = token.substring(eq+1, token.length()-1);
+        } else {
+          type = Type.PARAMETER;
+          name = token.substring(1, token.length()-1);
+          text = "";
+        }
+      }
+      return new Token(type, name, text);
+    }
+
+    @Override
+    public String toString() {
+      return this._type == Type.PARAMETER? '{'+this._name+'='+this._text+'}' : this._text;
+    }
+  }
+
+}
