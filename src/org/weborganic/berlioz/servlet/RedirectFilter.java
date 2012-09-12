c4967a12d2747b3aa91e0fec054c018e9d52403a
clauret <clauret@localhost>
2012-09-13 09:46:06 +1000

    Code style and format

--- src/org/weborganic/berlioz/servlet/RedirectFilter.java
+++ src/org/weborganic/berlioz/servlet/RedirectFilter.java
@@ -2,13 +2,14 @@
  * This file is part of the Berlioz library.
  *
  * For licensing information please see the file license.txt included in the release.
- * A copy of this licence can also be found at 
+ * A copy of this licence can also be found at
  *   http://www.opensource.org/licenses/artistic-license-2.0.php
  */
 package org.weborganic.berlioz.servlet;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -16,6 +17,7 @@ import java.util.Set;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -35,9 +37,9 @@ import org.weborganic.furi.URIResolver;
 
 /**
  * A basic filter to redirect URI patterns to other URI patterns.
- * 
+ *
  * <p>The redirect mapping can be specified as below:
- * 
+ *
  * <pre>{@code
  * <?xml version="1.0" encoding="utf-8"?>
  * <redirect-mapping>
@@ -48,15 +50,15 @@ import org.weborganic.furi.URIResolver;
  *   <redirect from="/{+path}.psml" to="/html/{+path}"/>
  * </redirect-mapping>
  * }</pre>
- * 
+ *
  * <p>Note: All redirects are currently temporary (302).
- * 
+ *
  * <p>See {@link #init(ServletConfig)} for details for configuration options.
- * 
+ *
  * @author Christophe Lauret (Weborganic)
  * @version 11 August 2010
  */
-@Beta public final class RedirectFilter implements Filter {
+@Beta public final class RedirectFilter implements Filter, Serializable {
 
   /**
    * As per requirement for the Serializable interface.
@@ -89,16 +91,17 @@ import org.weborganic.furi.URIResolver;
 
   /**
    * Initialises the Redirector Servlet.
-   * 
+   *
    * <p>This servlet accepts the following init parameters:
    * <ul>
    *   <li><code>config</code> path to the URI redirect mapping XML file (eg. '/config/redirect.xml')
    * </ul>
-   * 
+   *
    * @see javax.servlet.Servlet#init(javax.servlet.ServletConfig)
-   * 
+   *
    * @param config The filter configuration.
    */
+  @Override
   public void init(FilterConfig config) {
     // get the WEB-INF directory
     ServletContext context = config.getServletContext();
@@ -114,7 +117,7 @@ import org.weborganic.furi.URIResolver;
 
     File mappingFile = new File(webinfPath, mapping);
     if (!mappingFile.exists()) {
-      LOGGER.warn("'config' init-parameter points to non existing file {} - filter will have no effect", 
+      LOGGER.warn("'config' init-parameter points to non existing file {} - filter will have no effect",
       mappingFile.getAbsolutePath());
     }
 
@@ -124,6 +127,7 @@ import org.weborganic.furi.URIResolver;
   /**
    * Resets the target URL.
    */
+  @Override
   public void destroy() {
     this._mappingFile = null;
     this._mapping = null;
@@ -133,7 +137,8 @@ import org.weborganic.furi.URIResolver;
   /**
    * {@inheritDoc}
    */
-  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) 
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
     throws ServletException, IOException {
     if (req instanceof HttpServletRequest && res instanceof HttpServletResponse) {
       doHTTPFilter((HttpServletRequest)req, (HttpServletResponse)res, chain);
@@ -142,15 +147,15 @@ import org.weborganic.furi.URIResolver;
 
   /**
    * Do the filtering for a HTTP request.
-   * 
+   *
    * @param req   The HTTP servlet request.
    * @param res   The HTTP servlet response.
    * @param chain The filter chain.
-   * 
+   *
    * @throws IOException      Should an error occurs while writing the response.
    * @throws ServletException If thrown by the filter chain.
    */
-  public void doHTTPFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) 
+  public void doHTTPFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
       throws ServletException, IOException {
 
     // Load the config if needed
@@ -173,11 +178,11 @@ import org.weborganic.furi.URIResolver;
 
   /**
    * Actually performs the redirect.
-   * 
+   *
    * @param req   The HTTP servlet request.
    * @param res   The HTTP servlet response.
    * @param match The URI matched pattern.
-   * 
+   *
    * @throws IOException If thrown by the HTTP the response.
    */
   private void redirect(HttpServletRequest req, HttpServletResponse res, URIPattern match) throws IOException {
@@ -189,7 +194,7 @@ import org.weborganic.furi.URIResolver;
     URIResolver resolver = new URIResolver(from);
     URIResolveResult result = resolver.resolve(match);
 
-    // Expand the target URI with URI variables 
+    // Expand the target URI with URI variables
     Set<String> names = result.names();
     URIParameters parameters = new URIParameters();
     for (String name : names) {
@@ -212,7 +217,7 @@ import org.weborganic.furi.URIResolver;
 
   /**
    * Load the URI redirect configuration file.
-   * 
+   *
    * @return <code>true</code> if loaded correctly;
    *         <code>false</code> otherwise.
    */
@@ -234,7 +239,7 @@ import org.weborganic.furi.URIResolver;
 
   /**
    * Returns the HTML message when redirected including a link to the target page.
-   * 
+   *
    * @param url     The URL to redirect to.
    * @param encoded The encoded URL.
    * @return the HTML message when redirected.
