0aeac2288b5deb76706c737d20af256211956a5c
tyenki <tyenki@gmail.com>
2013-01-29 14:30:27 +1100

    Updated licence header

--- src/org/weborganic/berlioz/package-info.java
+++ src/org/weborganic/berlioz/package-info.java
@@ -1,4 +1,11 @@
+/*
+ * This file is part of the Berlioz library.
+ *
+ * For licensing information please see the file license.txt included in the release.
+ * A copy of this licence can also be found at
+ *   http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
 /**
- * High level classes used by Berlioz. 
+ * High level classes used by Berlioz.
  */
 package org.weborganic.berlioz;
