c9d636b16512902cfb0c3001e66206e1314aa3dd
tyenki <tyenki@gmail.com>
2013-12-17 12:43:37 +1100

    Added header

--- src/org/weborganic/berlioz/servlet/package-info.java
+++ src/org/weborganic/berlioz/servlet/package-info.java
@@ -1,4 +1,12 @@
+/*
+ * This file is part of the Berlioz library.
+ *
+ * For licensing information please see the file license.txt included in the release.
+ * A copy of this licence can also be found at
+ *   http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
 /**
+ *
  * Berlioz Servlets and related classes.
  */
 package org.weborganic.berlioz.servlet;
