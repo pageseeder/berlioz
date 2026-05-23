3a02eeb2f0a1831cfff0416985611276aa638316
clauret <clauret@weborganic.com>
2026-05-23 13:51:19 +1000

    Javadoc: add @throws to all JsonWriter interface methods
    JsonWriteFailureException (unchecked) is thrown on I/O errors by the
    Gson/Jackson/JSONP implementations but was not visible in the interface
    contract. Also documents IllegalStateException for end-context mismatches
    and NullPointerException for null string arguments.

--- pso-berlioz/src/main/java/org/pageseeder/berlioz/json/JsonWriter.java
+++ pso-berlioz/src/main/java/org/pageseeder/berlioz/json/JsonWriter.java
@@ -41,6 +41,7 @@ public interface JsonWriter extends AutoCloseable, Flushable {
    *
    * @param name The name of the array
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter startArray(String name);
 
@@ -48,12 +49,16 @@ public interface JsonWriter extends AutoCloseable, Flushable {
    * Start writing a JSON array in the context of an array.
    *
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter startArray();
 
   /**
-   * Writes the end of the current context.
+   * Writes the end of the current array context.
+   *
    * @return this instance.
+   * @throws IllegalStateException     if the current context is not an array.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter endArray();
 
@@ -62,77 +67,91 @@ public interface JsonWriter extends AutoCloseable, Flushable {
    *
    * @param name The name of the object
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter startObject(String name);
 
   /**
-   * Start writing a JSON object in the default context of an array.
+   * Start writing a JSON object in the context of an array or as a top-level value.
    *
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter startObject();
 
   /**
-   * Writes the end of the current context.
+   * Writes the end of the current object context.
+   *
    * @return this instance.
+   * @throws IllegalStateException     if the current context is not an object.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter endObject();
 
   /**
-   * Write a null value in the context of an object.
+   * Writes a JSON null value paired with the given name in the current object context.
    *
    * @param name a name in the JSON name/value pair to be written in current JSON object
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter nullValue(String name);
 
   /**
-   * Write a null value.
+   * Writes a JSON null value in the current array context.
    *
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter nullValue();
 
   /**
-   * Writes the specified value as a JSON value within the current array context.
+   * Writes the specified value as a JSON number within the current array context.
    *
    * @param number the value to write.
    * @return this instance.
+   * @throws IllegalArgumentException  if number is NaN or infinite.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter value(double number);
 
   /**
-   * Writes the specified value as a JSON value within the current array context.
+   * Writes the specified value as a JSON number within the current array context.
    *
    * @param number the value to write.
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter value(long number);
 
   /**
-   * Writes the specified value as a JSON value within the current array context.
+   * Writes the specified value as a JSON string within the current array context.
    *
    * @param value the value to write.
    * @return this instance.
+   * @throws NullPointerException      if value is null.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter value(String value);
 
   /**
-   * Writes the specified value as a JSON value within the current array context.
+   * Writes the specified value as a JSON boolean within the current array context.
    *
    * @param value the value to write.
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter value(boolean value);
 
   /**
-   * Writes a JSON name value pair in the current object context.
+   * Writes a JSON field name in the current object context.
    *
-   * <p>This method should always be followed by one of the {@link #value(String)} methods, the
-   * {@link #startObject()} method or the {@link #startArray()} method.</p>
+   * <p>Must be followed by one of the {@code value()}, {@link #startObject()}, or
+   * {@link #startArray()} methods.</p>
    *
    * @param name a name in the JSON name/value pair to be written in current JSON object
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter name(String name);
 
@@ -142,6 +161,8 @@ public interface JsonWriter extends AutoCloseable, Flushable {
    * @param name  the name in the JSON name/value pair to be written in current JSON object
    * @param value the value in the JSON name/value pair to be written in current JSON object
    * @return this instance.
+   * @throws NullPointerException      if value is null.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter field(String name, String value);
 
@@ -151,6 +172,7 @@ public interface JsonWriter extends AutoCloseable, Flushable {
    * @param name  the name in the JSON name/value pair to be written in current JSON object
    * @param value the value in the JSON name/value pair to be written in current JSON object
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter field(String name, boolean value);
 
@@ -160,6 +182,8 @@ public interface JsonWriter extends AutoCloseable, Flushable {
    * @param name  the name in the JSON name/value pair to be written in current JSON object
    * @param value the value in the JSON name/value pair to be written in current JSON object
    * @return this instance.
+   * @throws IllegalArgumentException  if value is NaN or infinite.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter field(String name, double value);
 
@@ -169,6 +193,7 @@ public interface JsonWriter extends AutoCloseable, Flushable {
    * @param name  the name in the JSON name/value pair to be written in current JSON object
    * @param value the value in the JSON name/value pair to be written in current JSON object
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   JsonWriter field(String name, long value);
 
@@ -177,6 +202,7 @@ public interface JsonWriter extends AutoCloseable, Flushable {
    *
    * @param map  a map of name/value pairs.
    * @return this instance.
+   * @throws JsonWriteFailureException if an I/O error occurs.
    */
   default JsonWriter properties(Map<String, String> map) {
     for (Map.Entry<String, String> field : map.entrySet()) {
@@ -194,13 +220,17 @@ public interface JsonWriter extends AutoCloseable, Flushable {
   boolean inObject();
 
   /**
-   * Closes this object and frees any resources associated with it.
+   * Closes this writer and flushes any buffered content to the underlying target.
+   *
+   * @throws JsonWriteFailureException if an I/O error occurs during close.
    */
   @Override
   void close();
 
   /**
-   * Flush any buffered content to itself and the underlying target.
+   * Flushes any buffered content to the underlying target.
+   *
+   * @throws JsonWriteFailureException if an I/O error occurs during flush.
    */
   @Override
   void flush();
