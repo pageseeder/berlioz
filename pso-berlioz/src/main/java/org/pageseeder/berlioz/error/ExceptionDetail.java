1d210f19b8481e6cc0038ea460a36e1ce1e338b0
clauret <clauret@weborganic.com>
2026-07-02 15:32:24 +1000

    Chore: Refactor ExceptionDetail to use source location separately

--- pso-berlioz/src/main/java/org/pageseeder/berlioz/error/ExceptionDetail.java
+++ pso-berlioz/src/main/java/org/pageseeder/berlioz/error/ExceptionDetail.java
@@ -57,25 +57,17 @@ public final class ExceptionDetail implements ProblemExtension {
   private final @Nullable String type;
   private final String message;
   private final @Nullable String stackTrace;
-  private final int locationLine;
-  private final int locationColumn;
-  private final @Nullable String locationPublicId;
-  private final @Nullable String locationSystemId;
+  private final @Nullable SourceLocation location;
   private final @Nullable ExceptionDetail cause;
 
   private ExceptionDetail(String className, @Nullable String type, String message,
-      @Nullable String stackTrace,
-      int locationLine, int locationColumn,
-      @Nullable String locationPublicId, @Nullable String locationSystemId,
+      @Nullable String stackTrace, @Nullable SourceLocation location,
       @Nullable ExceptionDetail cause) {
     this.className = className;
     this.type = type;
     this.message = message;
     this.stackTrace = stackTrace;
-    this.locationLine = locationLine;
-    this.locationColumn = locationColumn;
-    this.locationPublicId = locationPublicId;
-    this.locationSystemId = locationSystemId;
+    this.location = location;
     this.cause = cause;
   }
 
@@ -92,39 +84,19 @@ public final class ExceptionDetail implements ProblemExtension {
    * @return a new {@code ExceptionDetail}
    */
   public static ExceptionDetail of(Throwable ex, boolean includeStackTrace) {
-    // BerliozException is a framework transport wrapper; surface the real cause
-    if (ex instanceof org.pageseeder.berlioz.BerliozException && ex.getCause() != null) {
-      ex = ex.getCause();
-    }
-    String className = ex.getClass().getName();
-    String message = Errors.cleanMessage(ex);
-    String stackTrace = includeStackTrace ? Errors.getStackTrace(ex, true) : null;
-    ExceptionDetail cause = null;
-    if (includeStackTrace && ex.getCause() != null) {
-      cause = of(ex.getCause(), true);
-    }
+    ex = unwrapTransportException(ex);
+    String stackTrace = stackTrace(ex, includeStackTrace);
+    ExceptionDetail cause = cause(ex, includeStackTrace);
 
     if (ex instanceof SAXParseException) {
-      SAXParseException spe = (SAXParseException) ex;
-      String systemId = spe.getSystemId() != null ? Errors.toWebPath(spe.getSystemId()) : null;
-      return new ExceptionDetail(className, "SAXParseException", message, stackTrace,
-          spe.getLineNumber(), spe.getColumnNumber(), spe.getPublicId(), systemId, cause);
+      return fromSAXParseException((SAXParseException) ex, stackTrace, cause);
     }
 
     if (ex instanceof TransformerException) {
-      String typeAttr = ex instanceof TransformerConfigurationException
-          ? "TransformerConfigurationException" : "TransformerException";
-      SourceLocator loc = ((TransformerException) ex).getLocator();
-      int line = loc != null ? loc.getLineNumber() : -1;
-      int col  = loc != null ? loc.getColumnNumber() : -1;
-      String publicId  = loc != null ? loc.getPublicId() : null;
-      String systemId  = loc != null && loc.getSystemId() != null
-          ? Errors.toWebPath(loc.getSystemId()) : null;
-      return new ExceptionDetail(className, typeAttr, message, stackTrace,
-          line, col, publicId, systemId, cause);
+      return fromTransformerException((TransformerException) ex, stackTrace, cause);
     }
 
-    return new ExceptionDetail(className, null, message, stackTrace, -1, -1, null, null, cause);
+    return fromThrowable(ex, stackTrace, cause);
   }
 
   @Override
@@ -143,7 +115,7 @@ public final class ExceptionDetail implements ProblemExtension {
     out.field("class", this.className);
     out.optionalField("type", this.type);
     out.field("message", this.message, OutputWriter.FieldOption.XML_ELEMENT);
-    if (hasLocation()) writeLocationTo(out);
+    if (this.location != null) this.location.writeTo(out);
     out.optionalField("stack-trace", this.stackTrace, OutputWriter.FieldOption.XML_ELEMENT);
     if (this.cause != null) {
       out.startObject("cause");
@@ -152,20 +124,103 @@ public final class ExceptionDetail implements ProblemExtension {
     }
   }
 
-  private void writeLocationTo(OutputWriter out) {
-    out.startObject("location");
-    if (this.locationLine != -1) out.field("line", this.locationLine);
-    if (this.locationColumn != -1) out.field("column", this.locationColumn);
-    out.optionalField("public-id", this.locationPublicId);
-    out.optionalField("system-id", this.locationSystemId);
-    out.endObject();
-  }
-
   // --- Private helpers -------------------------------------------------------------------------
 
-  private boolean hasLocation() {
-    return this.locationLine != -1 || this.locationColumn != -1
-        || this.locationPublicId != null || this.locationSystemId != null;
+  private static Throwable unwrapTransportException(Throwable ex) {
+    // BerliozException is a framework transport wrapper; surface the real cause.
+    if (ex instanceof org.pageseeder.berlioz.BerliozException && ex.getCause() != null) {
+      return ex.getCause();
+    }
+    return ex;
+  }
+
+  private static @Nullable String stackTrace(Throwable ex, boolean includeStackTrace) {
+    return includeStackTrace ? Errors.getStackTrace(ex, true) : null;
+  }
+
+  private static @Nullable ExceptionDetail cause(Throwable ex, boolean includeStackTrace) {
+    return includeStackTrace && ex.getCause() != null ? of(ex.getCause(), true) : null;
+  }
+
+  private static ExceptionDetail fromSAXParseException(SAXParseException ex,
+      @Nullable String stackTrace, @Nullable ExceptionDetail cause) {
+    return new ExceptionDetail(className(ex), "SAXParseException", message(ex), stackTrace,
+        SourceLocation.of(ex), cause);
+  }
+
+  private static ExceptionDetail fromTransformerException(TransformerException ex,
+      @Nullable String stackTrace, @Nullable ExceptionDetail cause) {
+    return new ExceptionDetail(className(ex), transformerType(ex), message(ex), stackTrace,
+        SourceLocation.of(ex.getLocator()), cause);
+  }
+
+  private static ExceptionDetail fromThrowable(Throwable ex,
+      @Nullable String stackTrace, @Nullable ExceptionDetail cause) {
+    return new ExceptionDetail(className(ex), null, message(ex), stackTrace, null, cause);
+  }
+
+  private static String className(Throwable ex) {
+    return ex.getClass().getName();
+  }
+
+  private static String message(Throwable ex) {
+    return Errors.cleanMessage(ex);
+  }
+
+  private static String transformerType(TransformerException ex) {
+    return ex instanceof TransformerConfigurationException
+        ? "TransformerConfigurationException" : "TransformerException";
+  }
+
+  private static final class SourceLocation {
+
+    private final int line;
+    private final int column;
+    private final @Nullable String publicId;
+    private final @Nullable String systemId;
+
+    private SourceLocation(int line, int column, @Nullable String publicId,
+        @Nullable String systemId) {
+      this.line = line;
+      this.column = column;
+      this.publicId = publicId;
+      this.systemId = systemId;
+    }
+
+    private static @Nullable SourceLocation of(SAXParseException ex) {
+      return of(ex.getLineNumber(), ex.getColumnNumber(), ex.getPublicId(),
+          toWebPath(ex.getSystemId()));
+    }
+
+    private static @Nullable SourceLocation of(@Nullable SourceLocator loc) {
+      if (loc == null) return null;
+      return of(loc.getLineNumber(), loc.getColumnNumber(), loc.getPublicId(),
+          toWebPath(loc.getSystemId()));
+    }
+
+    private static @Nullable SourceLocation of(int line, int column,
+        @Nullable String publicId, @Nullable String systemId) {
+      if (!hasFields(line, column, publicId, systemId)) return null;
+      return new SourceLocation(line, column, publicId, systemId);
+    }
+
+    private void writeTo(OutputWriter out) {
+      out.startObject("location");
+      if (this.line != -1) out.field("line", this.line);
+      if (this.column != -1) out.field("column", this.column);
+      out.optionalField("public-id", this.publicId);
+      out.optionalField("system-id", this.systemId);
+      out.endObject();
+    }
+
+    private static @Nullable String toWebPath(@Nullable String systemId) {
+      return systemId != null ? Errors.toWebPath(systemId) : null;
+    }
+
+    private static boolean hasFields(int line, int column,
+        @Nullable String publicId, @Nullable String systemId) {
+      return line != -1 || column != -1 || publicId != null || systemId != null;
+    }
   }
 
 }
